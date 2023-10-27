7af7b26068d7fddf50a86b311722862719b553d0
root <root@prometheus>
2023-10-27 15:42:37 +0000

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,19 @@
 import { useContext } from "react";
 import { ThemeContext } from "../App";
 import { LogoIcon, MoonIcon, SunIcon, PrometheusIcon } from "./assets/icons";
+import prometheusIcon from "./assets/prometheus.svg";
 
 const Header = () => {
 
 const { isDark, setIsDark } = useContext(ThemeContext);
-const prometheusIcon = require('./assets/prometheus.svg')
+## const prometheusIcon = require('./assets/prometheus.svg')
   
 return (
     <header className="header">
       <div className="container">
         <div className="logo-wrapper">
           <div className="logo">
-	  <img src={{prometheusIcon}} />
+	  <img src={prometheusIcon} />
             <span>Prometheus</span>
           </div>
           <button onClick={() => setIsDark(!isDark)} className="icon">{isDark ? <MoonIcon /> : <SunIcon />}</button>
