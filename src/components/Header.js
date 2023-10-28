d2c49c7c854f391dbb5ad0b293e9ef386618352f
root <root@prometheus>
2023-10-28 06:21:48 +0000

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ return (
           <div className="logo">
 	  <img src={prometheusIcon} width="100" height="50"/>
 	  <PrometheusIcon />
-            <span style={{fontSize:16}}>Prometheus</span>
+            <span className="title">Prometheus</span>
           </div>
           <button onClick={() => setIsDark(!isDark)} className="icon">{isDark ? <MoonIcon /> : <SunIcon />}</button>
         </div>
